ea7d34cc318dd422159f04ff5e6ecf1fe0d04a36
rajesh <rajwaitu@gmail.com>
2019-11-18 16:34:39 +0530

    endpoint changes

--- src/main/java/com/techrocking/customer/controller/CustomerController.java
+++ src/main/java/com/techrocking/customer/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techrocking.customer.data.converter.CustomerDataConverter;
@@ -19,6 +20,7 @@ import com.techrocking.customer.service.CustomerService;
 
 @RefreshScope
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 	
 	@Autowired
